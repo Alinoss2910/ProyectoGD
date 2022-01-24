41fb12806ba0efd2a7f913c9c0ba3579898529f7
Airam <alinoss2910@g.educaand.es>
2022-01-24 10:40:53 +0100

    ALO-Salto dse jugador

--- src/main/java/es/airamlinares/gd/App.java
+++ src/main/java/es/airamlinares/gd/App.java
@@ -4,10 +4,13 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -20,8 +23,12 @@ import javafx.util.Duration;
  */
 public class App extends Application {
     Group groupPincho;
+    Group groupCubo;
     int posPinX = 590;
     final int POSYPIN = 430;
+    int posXJug = 100;
+    int posYJug = 435;
+    int velSalto;
 
     @Override
     public void start(Stage stage) {
@@ -32,25 +39,37 @@ public class App extends Application {
         stage.setTitle("Geometry Dash");
         stage.setScene(scene);
         stage.show();
-        //Definir grupo pinchos
+        //Definir grupos
         groupPincho = new Group();
+        groupCubo = new Group();
         //Cargar imagen del panel principal
         Image img = new Image(getClass().getResourceAsStream("/images/fondo1.jpg"));
         //Imagen del pincho
         Image imgPincho = new Image(getClass().getResourceAsStream("/images/pincho.png"));
+        //Imagen Jugador
+        Image imgCubo = new Image(getClass().getResourceAsStream("/images/cubo.png"));
         ImageView imgView = new ImageView(img);
         ImageView imgViewPin = new ImageView(imgPincho);
+        ImageView imgViewCubo = new ImageView(imgCubo);
         //Poner la imagen de fondo
         paneRoot.getChildren().add(imgView);
+        //Define objeto pincho
         Rectangle pincho = new Rectangle(posPinX, POSYPIN, 50, 50);
         pincho.setFill(Color.TRANSPARENT);
         groupPincho.getChildren().add(pincho);
         groupPincho.getChildren().add(imgViewPin);
-        //Añade a la pantalla el grupo
+        //Define Jugador
+        Rectangle jugador = new Rectangle(posXJug, posYJug, 50, 50);
+        groupCubo.getChildren().add(jugador);
+        groupCubo.getChildren().add(imgViewCubo);
+        //Añade a la pantalla los grupos
         paneRoot.getChildren().add(groupPincho);
+        paneRoot.getChildren().add(groupCubo);
         //Posicion inicial del pincho
         groupPincho.setLayoutY(POSYPIN);
         groupPincho.setLayoutX(posPinX);
+        groupCubo.setLayoutY(posYJug);
+        groupCubo.setLayoutX(posXJug);
         //Timeline de movimiento pincho
         Timeline movPincho = new Timeline(
                 new KeyFrame(Duration.seconds(0.005), (ActionEvent ae) -> {
@@ -64,8 +83,26 @@ public class App extends Application {
         );
         movPincho.setCycleCount(Timeline.INDEFINITE);
         movPincho.play();
-        
-        
+        //Salto del jugador
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            Timeline movJug = new Timeline(
+                new KeyFrame(Duration.seconds(0.005), (ActionEvent ae) -> {
+                    posYJug += velSalto;
+                    if(event.getCode() == KeyCode.UP) { //Si pulsas arriba el jugador salta
+                        velSalto = -1;
+                        groupCubo.setLayoutY(posYJug);
+                    }
+                    if(posYJug ==370) {
+                        velSalto = 1;
+                    }
+                })
+            );
+            movJug.setCycleCount(Timeline.INDEFINITE);
+            movJug.play();
+        });
+        if(posYJug == 435) {
+            velSalto = 0;
+        }
     }
 
     public static void main(String[] args) {
